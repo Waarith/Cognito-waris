a3f05ac11411bbc28e8ebb5125e31501dcb22d2f
Jimoh Abdulwaris <156073614+Waarith@users.noreply.github.com>
2024-06-23 15:19:29 +0100

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      fontFamily: {
+        'lato': ["Lato", "sans-serif"],
+        "gilroy-heavy": ["Gilroy-Heavy ☞"],
+        "gilroy-bold": ["Gilroy-Bold ☞"],
+        "gilroy-light": ["Gilroy-Light ☞"],
+        "gilroy-medium": ["Gilroy-Medium ☞"],
+        "gilroy-regular": ["Gilroy-Regular ☞"],
+      },
+      backgroundImage: {
+        'data-bg': "url('/images/data-bg.svg')",
+      },
+      animation: {
+        marquee: "marquee 8s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+    },
   plugins: [],
 }
-
+}
